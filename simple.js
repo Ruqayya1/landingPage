8bef0281f086d674280aa3674d72b9f1b9d321ae
Ruqayya1 <ruqayya.9428@gmail.com>
2018-10-20 00:38:28 -0500

    JavaScript Practice

---
+++ simple.js
@@ -0,0 +1,35 @@
+let val;
+val = 555;
+console.log(val);
+val =(5).toString();
+console.log(val);
+val = parseInt ('202.89');
+console.log(val);
+let  sum = 467.89+567.68;
+console.log(typeof sum);
+
+// 
+let num;
+num = Math.PI;
+num = Math.round(67.9);
+
+// output
+console.log(num);
+
+let name;
+let firstname = "Mary";
+let lastname ="sen";
+
+name ="Ruqayya";
+name+="jabeen";
+name = firstname.concat(' ', lastname);
+
+name = firstname.slice(-3);
+
+let str = "My profession is developer";
+
+name = str.split(' ');
+
+name = str.includes("allhumdulliah");
+
+console.log(name);
